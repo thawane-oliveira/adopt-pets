511cee48d903ea8ba0896710b4825eff38d467ef
Thawane <thawaneapoliveira@gmail.com>
2023-02-22 16:47:07 -0300

    corrige erro de português

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -11,7 +11,7 @@ function AboutUs() {
       <h2 className='about-title'>Sobre nós</h2>
       <p className='about-text'>
       A adote um pet nasceu do sonho de um grupo de amigos apaixonados por animais e com o sentimento mútuo de que era necessário fazer justiça por aqueles que eram vítimas de maus tratos ou abandono.
-      Queremos incentivar o ato de adoção, divulgar informações sobre posse responsável e também particiar da redução do índice de animais abandonados.
+      Queremos incentivar o ato de adoção, divulgar informações sobre posse responsável e também particiar da redução no índice de animais abandonados.
       </p>
       <h2 className='about-values'>Nossos valores</h2>
     <ul className='about-list'>
